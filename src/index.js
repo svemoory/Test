2963084a6f87b7174f6853cec708690f464132fa
Surekha Vemoory <svemoory@mlslistings.com>
2017-05-08 16:21:06 -0700

    test

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import MLSOData from './MLSOData'
@@ -7,4 +7,16 @@ import './index.css';
 ReactDOM.render(
   <App />,
   document.getElementById('root')
-);
+);*/
+
+'use strict';
+
+/**
+ * Adds commas to a number
+ * @param {number} number
+ * @param {string} locale
+ * @return {string}
+ */
+module.exports = function(number, locale) {
+    return number.toLocaleString(locale);
+};
